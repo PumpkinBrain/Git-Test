602982205b92ca7ba3e04a41bfd17d528c30287f
PumpkinBrain <dmcalmeida@outlook.com>
2021-09-11 17:16:19 -0300

    added loop functionality and changed display

--- Calculator/src/calculator/Calculus.java
+++ Calculator/src/calculator/Calculus.java
@@ -2,35 +2,48 @@ package calculator;
 
 public class Calculus {
 	Vars var = new Vars();
-	
+
+//first value will be inserted one time, then a while loop will run
+//so that the second value and the operation can be input over and over
+//then the results will be stored in the first variable so the calculus can continue
+//until the user wishes so
 	protected void inputs() {
 		var.setA(Input.nextInt("Digite o primeiro numero"));
-		var.setB(Input.nextInt("Digite o segundo numero"));
-		var.setc(Input.nextChar("Digite a operacao"));
+		
+		while (var.getC() != '=') {
+			var.setc(Input.nextChar("Digite a operacao"));
+			var.setB(Input.nextInt("Digite o segundo numero"));
+			logic();
+		}
 	}
 	
+//check for the operation and execute corresponding calculus
 	protected void logic() {
 		switch(var.getC()) {
 		case '+':
 			var.setA(var.getA() + var.getB());
-			System.out.println(var.getA());
+			System.out.println("resultado: " + var.getA());
 			break;
 		case '-':
 			var.setA(var.getA() - var.getB());
-			System.out.println(var.getA());
+			System.out.println("resultado: " + var.getA());
 			break;
 		case '*':
 			var.setA(var.getA() * var.getB());
-			System.out.println(var.getA());
+			System.out.println("resultado: " + var.getA());
 			break;
 		case '/':
 			var.setA(var.getA() / var.getB());
-			System.out.println(var.getA());
+			System.out.println("resultado: " + var.getA());
 			break;
 		case '^':
 			var.setA(var.pow(var.getA(), var.getB()));
-			System.out.println(var.getA());
+			System.out.println("resultado: " + var.getA());
 			break;
+		}
+//when user decides it's over, print the final result
+		if(var.getC() == '=') {
+			System.out.println("resultado final: " + var.getA());
 		}
 	}
 }
